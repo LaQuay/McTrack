fdb96e64f5c71bac597a6120f231fbdd31ba43fd
Marc Vila <marcvilagomez@gmail.com>
2017-06-18 07:27:07 +0200

    Runnables to start/end videos

--- app/app/src/main/java/dev/juanmarc/tracking/mctrackdemo/MainFragment.kt
+++ app/app/src/main/java/dev/juanmarc/tracking/mctrackdemo/MainFragment.kt
@@ -1,6 +1,9 @@
 package dev.juanmarc.tracking.mctrackdemo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,23 +17,64 @@ import com.github.kittinunf.fuel.Fuel
 
 class MainFragment : Fragment() {
     var TAG: String = "MainFragment" //TODO Do with TAGNAME
+    private val TIME_FOR_NEW_AD: Long = 5000
+    private val STOP_VIDEO_MILIS: Long = 10000
+    private val BLACK_SCREEN_YOUTUBE: String = "https://www.youtube.com/watch?v=XIMLoLxmTDw"
+    private var lastURL: String = ""
+
+    private var handler: Handler = Handler()
+    private lateinit var runnableNewUrlBackground: Runnable
+    private lateinit var runnableStopVideo: Runnable
+
+    init {
+        runnableNewUrlBackground = Runnable {
+            testHTTP()
+            handler.postDelayed(runnableNewUrlBackground, TIME_FOR_NEW_AD)
+        }
+
+        runnableStopVideo = Runnable {
+            startYoutubeIntent(BLACK_SCREEN_YOUTUBE)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lastURL = ""
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handler.removeCallbacks(runnableNewUrlBackground)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
-        testHTTP()
+        getWebsiteUrl()
 
         return view
     }
 
-    fun testHTTP() {
+    fun getWebsiteUrl() {
+        handler.post(runnableNewUrlBackground)
+    }
+
+    fun startYoutubeIntent(url: String) {
+        handler.postDelayed(runnableStopVideo, STOP_VIDEO_MILIS)
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    }
+
+    private fun testHTTP() {
         Fuel.get("http://httpbin.org/get").responseString { request, response, result ->
             result.fold({ d ->
-                //do something with data
                 Log.e(TAG, d)
 
+                val parsedURL = "https://www.youtube.com/watch?v=5RNkQaDcRAc"
+                if (parsedURL != lastURL) {
+                    lastURL = parsedURL
+                    startYoutubeIntent(parsedURL)
+                }
             }, { err ->
-                //do something with error
                 Log.e(TAG, err.toString())
             })
         }
