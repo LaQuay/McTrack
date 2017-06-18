1b45172f4d651ba52df04c0e75720c6abef4ef72
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-06-18 12:25:51 +0200

    coordinate generator need to be implemented

--- macgenerator/runGenerator.py
+++ macgenerator/runGenerator.py
@@ -1,14 +1,56 @@
+import json
 import random
 import threading
 import time
 
 import requests
-import thread
 
-topic_list = [
+type_list = [
     "sports", "coworking", "nike", "decathlon", "food", "technology", "real madrid", "coca-cola"
 ]
 mac_list = []
+
+
+class ProbeGeneratorThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        generate_probe()
+
+
+class NearbyCoordinatesGenerator:
+
+    def generate_nearby_to(self, location):
+        #TODO: IMPLEMENT HERE LAQUAY
+        lat = location['lat']
+        lng = location['lng']
+        radius = 200
+        rangeX = (0, 2500)
+        rangeY = (0, 2500)
+        qty = 10  # or however many points you want
+
+        # Generate a set of all points within 200 of the origin, to be used as offsets later
+        # There's probably a more efficient way to do this.
+        deltas = set()
+        for x in range(-radius, radius + 1):
+            for y in range(-radius, radius + 1):
+                if x * x + y * y <= radius * radius:
+                    deltas.add((x, y))
+
+        randPoints = []
+        excluded = set()
+        i = 0
+        while i < qty:
+            x = random.randrange(*rangeX)
+            y = random.randrange(*rangeY)
+            if (x, y) in excluded: continue
+            randPoints.append((x, y))
+            i += 1
+            excluded.update((x + dx, y + dy) for (dx, dy) in deltas)
+
+        print(randPoints)
+        return randPoints
 
 
 def generate_random_mac():
@@ -35,33 +77,47 @@ def pick_random_mac():
     return random.choice(mac_list)
 
 
-class GenerateProbeThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        generate_probe()
+def generate_probe_chunk_threaded(chunk_size):
+    for i in range(0, chunk_size):
+        new_thread = ProbeGeneratorThread()
+        new_thread.start()
 
 
 def generate_probe_chunk(chunk_size):
     for i in range(0, chunk_size):
-        new_thread = GenerateProbeThread()
-        new_thread.start()
+        generate_probe()
 
 
-def pick_topics():
-    num_topics = random.randint(1, len(topic_list))
-    return random.sample(topic_list, num_topics)
+def pick_random_types():
+    num_types = random.randint(1, len(type_list))
+    return random.sample(type_list, num_types)
 
 
 def generate_nearby_location():
-    pass
+    return {"lat": 50.088080, "lng": 14.420406}
+
+
+def get_types_from_gmaps(location):
+    types = []
+    lat = str(location['lat'])
+    lng = str(location['lng'])
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + \
+          "location=" + lat + ',' + lng + \
+          "&radius=50&type=establishment&key=AIzaSyC0N7To1nSSIqAVHIvbs0FpLa3gIFfhP0k"
+    response = requests.get(url)
+    establishments = json.loads(response.content)['results']
+    for element in establishments:
+        # non repeated
+        types += list(set(element['types']) - set(types))
+
+    return types
 
 
 def generate_probe():
     random_mac = pick_random_mac()
     timestamp = int(time.time())
-    topics = pick_topics()
+    location = generate_nearby_location()
+    types = get_types_from_gmaps(location)
     url = "https://mctrack-6a99b.firebaseio.com/customers/" + \
           str(random_mac) + \
           "/probes/" + \
@@ -69,31 +125,22 @@ def generate_probe():
           ".json" + \
           "?auth=XyGsxNV6kJdDSh4PxPKApPpwi6n051YqVao4uLfV"
     payload = {
-        "location": {
-            "features": [
-                {
-                    "geometry": {
-                        "coordinates": [
-                            14.442162830382586,
-                            50.07002479852162
-                        ],
-                        "type": "Point"
-                    },
-                    "type": "Feature"
-                }
-            ],
-            "type": "FeatureCollection"
-        },
+        "location": location,
         "timestamp": timestamp,
-        "topics": topics
+        "types": types
     }
     status = requests.put(url, json=payload)
     print(random_mac + " " + str(status.status_code))
 
 
 def main():
-    generate_mac_list(50)
-    generate_probe_chunk(1000)
+    get_types_from_gmaps(generate_nearby_location())
+    gen = NearbyCoordinatesGenerator()
+    gen.generate_nearby_to(generate_nearby_location())
+
+    # generate_mac_list(50)
+    # generate_probe_chunk_threaded(300)
+    # generate_probe_chunk(1000)
 
 
 if __name__ == "__main__":
