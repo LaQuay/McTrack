a3256831c4dfa6f074f4bfc7c976e2f927b0f6b0
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-06-18 06:52:44 +0200

    MAC generator working

--- macgenerator/runGenerator.py
+++ macgenerator/runGenerator.py
@@ -1,7 +1,10 @@
 import random
+import time
+
+import requests
 
 
-def rand_mac():
+def generate_random_mac():
     return "%02x:%02x:%02x:%02x:%02x:%02x" % (
         random.randint(0, 255),
         random.randint(0, 255),
@@ -12,10 +15,49 @@ def rand_mac():
     )
 
 
+def generate_probe_chunk(chunk_size):
+    for i in range(0, chunk_size):
+        generate_probe()
+
+
+def generate_probe():
+    random_mac = generate_random_mac()
+    url = "https://mctrack-6a99b.firebaseio.com/customers/" + str(
+        random_mac) + ".json?auth=XyGsxNV6kJdDSh4PxPKApPpwi6n051YqVao4uLfV"
+    timestamp = int(time.time())
+    payload = {
+        "probes": [
+            {
+                "location": {
+                    "features": [
+                        {
+                            "geometry": {
+                                "coordinates": [
+                                    14.442162830382586,
+                                    50.07002479852162
+                                ],
+                                "type": "Point"
+                            },
+                            "type": "Feature"
+                        }
+                    ],
+                    "type": "FeatureCollection"
+                },
+                "timestamp": timestamp,
+                "topics": [
+                    {"name": "sports"},
+                    {"name": "coworking"},
+                    {"name": "technology"}
+                ]
+            }
+        ]
+    }
+    status = requests.put(url, json=payload)
+    print(random_mac + " " + str(status.status_code))
+
+
 def main():
-    print("hello world")
-    for i in range(0, 20):
-        print(rand_mac())
+    generate_probe_chunk(5)
 
 
 if __name__ == "__main__":
